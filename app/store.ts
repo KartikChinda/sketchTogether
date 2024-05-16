02f5c99da87a41a51487b72fbac28e8b2a5e8f91
Kartik Chinda <kartik.chinda@gmail.com>
2024-05-17 05:05:09 +0530

    Fetched data and updated toolbox on the basis of the active button

--- app/store.ts
+++ app/store.ts
@@ -1,6 +1,12 @@
 "use client";
 import { configureStore } from "@reduxjs/toolkit";
+import MenuReducer from './slice/menuSlice';
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        menu: MenuReducer,
+    }
 })
+
+// this is solely for typescript purposes. In useSelectorHook, you are supposed to add in the type of state. Or give any, if you dont want to be hired. 
+export type RootState = ReturnType<typeof store.getState>
