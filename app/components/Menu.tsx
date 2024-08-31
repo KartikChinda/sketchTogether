2b735a481e9866e7114b5390f67adf984dd31aa2
Kartik Chinda <kartik.chinda@gmail.com>
2024-09-01 00:23:45 +0530

    Cat walked across my keyboard.

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -31,7 +31,7 @@ const MenuCard = () => {
 
     return (
         <div className='absolute top-10 bg-[#23120B] w-[1/4] left-1/2 rounded-md px-2 py-1 duration-75 translate-x-[-50%] flex flex-row md:gap-10 gap-5 hover:px-5 hover:py-2 justify-between items-center shadow-xl shadow-[#21209C]'>
-            <div className='h-[40px] text-3xl flex justify-center items-center hover:cursor-pointer hover:bg-[#21209C] text-[#FDB827] rounded-md m-2 w-[40px]' onClick={() => handleItemClicked(menuItems.PENCIL)} style={clickedItem === "pe1517ncil" ? { backgroundColor: "#21209C" } : {}}><FaPen /></div>
+            <div className='h-[40px] text-3xl flex justify-center items-center hover:cursor-pointer hover:bg-[#21209C] text-[#FDB827] rounded-md m-2 w-[40px]' onClick={() => handleItemClicked(menuItems.PENCIL)} style={clickedItem === "pencil" ? { backgroundColor: "#21209C" } : {}}><FaPen /></div>
             <div className="h-[40px] text-3xl flex justify-center items-center hover:cursor-pointer hover:bg-[#21209C] text-[#FDB827] rounded-md m-2 w-[40px]" onClick={() => handleItemClicked(menuItems.ERASER)} style={clickedItem === "eraser" ? { backgroundColor: "#21209C" } : {}}><FaEraser /></div>
             <div className='h-[40px] text-3xl flex justify-center items-center hover:cursor-pointer hover:bg-[#21209C] text-[#FDB827] rounded-md m-2 w-[40px]' onClick={() => handleActionMenuItem(menuItems.UNDO)}><FaUndo /></div>
             <div className='h-[40px] text-3xl flex justify-center items-center hover:cursor-pointer hover:bg-[#21209C] text-[#FDB827] rounded-md m-2 w-[40px]' onClick={() => handleActionMenuItem(menuItems.REDO)}><FaRedo /></div>
